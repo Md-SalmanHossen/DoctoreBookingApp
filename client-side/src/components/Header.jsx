a88d866d4bbe37498e81638f5062be8dc4f39d1e
SalmanHossen <hossainsalman873@gmail.com>
2024-10-19 11:06:53 +0600

    done doctor special section

--- client-side/src/components/Header.jsx
+++ client-side/src/components/Header.jsx
@@ -4,13 +4,18 @@ import { assets } from '../assets/assets_frontend/assets';
 const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap rounded-lg px-6 md:px-10 lg:px-20 bg-primary'>
+      
       {/* Left Side Content */}
       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
         <h1 className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
           Book Appointment <br /> With Trusted Doctors
         </h1>
         <div className='flex flex-col md:flex-row gap-3 text-sm text-white items-center font-light'>
-          <img className='w-28' src={assets.group_profiles} alt="Profiles of trusted doctors" />
+          <img 
+            className='w-28' 
+            src={assets.group_profiles} 
+            alt="Profiles of trusted doctors" 
+          />
           <p>
             Simply browse through an extensive list of trusted doctors, 
             <br className='hidden sm:block' />
