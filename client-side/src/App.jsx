9f9db9b5d192bf8414e85deda99b14854ed2fdd4
SalmanHossen <hossainsalman873@gmail.com>
2024-10-19 13:53:03 +0600

    footer section add

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -14,6 +14,7 @@ import Navbar from './components/Navbar';
 //package import
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
         <Route path="/my-appointment" element={<MyAppointment/>}/>
         <Route path="/appointment/:docId" element={<Appointment/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
